85a1fea31b0152357af0d7b649575552904f0321
erwin.io <erwinramirez220@gmail.com>
2023-10-15 04:38:35 +0800

    api

--- api/src/server.ts
+++ api/src/server.ts
@@ -12,26 +12,39 @@ import * as cors from "cors";
 
 const app: Express = express();
 
-// Add a list of allowed origins.
-// If you have more origins you would like to add, you can add them to the array below.
-const allowedOrigins = ['http://localhost:3000', 'https://market-stall-rentals-web.vercel.app'];
+// Parsing the env file.
+if (!process.env.NODE_ENV) {
+  dotenv.config({ path: path.resolve(__dirname, "./envs/development.env") });
+}
 
+//options for cors midddleware
 const options: cors.CorsOptions = {
-  origin: allowedOrigins
+  allowedHeaders: [
+    'Origin',
+    'X-Requested-With',
+    'Content-Type',
+    'Accept',
+    'X-Access-Token',
+  ],
+  credentials: true,
+  methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+  origin: "https://market-stall-rentals-web.vercel.app",
+  preflightContinue: false,
 };
 
-// Then pass these options to cors:
-app.use(cors());
+//use cors middleware
+app.use(cors(options));
+
+//add your routes
+
+//enable pre-flight
+app.options('*', cors(options));
 
 /** Parse the request */
 app.use(express.urlencoded({ extended: false }));
 /** Takes care of JSON data */
 app.use(express.json());
 
-// Parsing the env file.
-if (!process.env.NODE_ENV) {
-  dotenv.config({ path: path.resolve(__dirname, "./envs/development.env") });
-}
 const dbConfig = typeOrmConfig.createConfig();
 
 createConnection(dbConfig)
@@ -40,6 +53,22 @@ createConnection(dbConfig)
   })
   .catch((error) => console.log("TypeORM connection error: ", error));
 
+// /** RULES OF OUR API */
+// app.use((req, res, next) => {
+//   // set the CORS policy
+//   res.header("Access-Control-Allow-Origin", "*");
+//   // set the CORS headers
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "origin, X-Requested-With,Content-Type,Accept, Authorization",
+//   );
+//   // set the CORS method headers
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
 /** Routes */
 const routePrefix = "api/";
