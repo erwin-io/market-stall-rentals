3a39f32bf4e2d262fcc45a791a94aba2f6a97fb1
erwin.io <erwinramirez220@gmail.com>
2023-10-15 23:28:49 +0800

    cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -17,6 +17,13 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 }, cors({ maxAge: 84600, origin: "*" }));
 app.options('*', cors()) // include before other routes
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*'); //<-- you can change this with a specific url like http://localhost:4200
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 // Parsing the env file.
 if (!process.env.NODE_ENV) {
   dotenv.config({ path: path.resolve(__dirname, "./envs/development.env") });
