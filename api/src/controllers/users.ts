07bc68a013478a59c32e4260d37c15ef7fa8025f
erwin.io <erwinramirez220@gmail.com>
2023-10-16 01:23:05 +0800

    metadata issue

--- api/src/controllers/users.ts
+++ api/src/controllers/users.ts
@@ -3,9 +3,9 @@ import { Router, Request, Response, NextFunction } from "express";
 import axios, { AxiosResponse } from "axios";
 import { Users } from "../db/entities/Users";
 import { In, getManager, getRepository } from "typeorm";
-import { compare, hash } from "../utils/utils";
 import { Roles } from "../db/entities/Roles";
 import { CONST_USERTYPE } from "../utils/constant";
+import { compare, hash } from "../utils/utils";
 var cors = require('cors')
 
 export const usersRouter = Router();
