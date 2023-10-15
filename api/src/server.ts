02ed69718ddd279dae2027a39ea49479be291009
erwin.io <erwinramirez220@gmail.com>
2023-10-15 23:06:46 +0800

    cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -31,22 +31,22 @@ createConnection(dbConfig)
   })
   .catch((error) => console.log("TypeORM connection error: ", error));
 
-// /** RULES OF OUR API */
-// app.use((req, res, next) => {
-//   // set the CORS policy
-//   res.header("Access-Control-Allow-Origin", "*");
-//   // set the CORS headers
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "origin, X-Requested-With,Content-Type,Accept, Authorization",
-//   );
-//   // set the CORS method headers
-//   if (req.method === "OPTIONS") {
-//     res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
-//     return res.status(200).json({});
-//   }
-//   next();
-// });
+/** RULES OF OUR API */
+app.use((req, res, next) => {
+  // set the CORS policy
+  res.header("Access-Control-Allow-Origin", "*");
+  // set the CORS headers
+  res.header(
+    "Access-Control-Allow-Headers",
+    "origin, X-Requested-With,Content-Type,Accept, Authorization",
+  );
+  // set the CORS method headers
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET PATCH DELETE POST");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 /** Routes */
 const routePrefix = "api/";
